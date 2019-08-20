a01858508878839842676bcb65ae8e735e7cf3ef
Vali Mandeal <mandeal.valeriu@gmail.com>
2019-08-20 12:55:35 +0300

    Adding a Modal Component

--- DatingApp-SPA/src/app/admin/user-management/user-management.component.ts
+++ DatingApp-SPA/src/app/admin/user-management/user-management.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from 'src/app/_models/user';
 import { AdminService } from 'src/app/_services/admin.service';
+import { BsModalService, BsModalRef } from 'ngx-bootstrap';
+import { RolesModalComponent } from '../roles-modal/roles-modal.component';
 
 @Component({
   selector: 'app-user-management',
@@ -9,8 +11,10 @@ import { AdminService } from 'src/app/_services/admin.service';
 })
 export class UserManagementComponent implements OnInit {
   users: User[];
+  bsModalRef: BsModalRef;
 
-  constructor(private adminService: AdminService) { }
+  constructor(private adminService: AdminService,
+              private modalService: BsModalService) { }
 
   ngOnInit() {
     this.getUsersWithRoles();
@@ -22,5 +26,19 @@ export class UserManagementComponent implements OnInit {
     }, error => {
       console.log(error);
     });
+  }
+
+  editRolesModal() {
+    const initialState = {
+      list: [
+        'Open a modal with component',
+        'Pass your data',
+        'Do something else',
+        '...'
+      ],
+      title: 'Modal with component'
+    };
+    this.bsModalRef = this.modalService.show(RolesModalComponent, {initialState});
+    this.bsModalRef.content.closeBtnName = 'Close';
   }
 }
